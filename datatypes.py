a645a4744918bc7846c5e6717078536e7ab6c525
Niketatiwari <111993600+Niketatiwari@users.noreply.github.com>
2022-08-30 20:03:29 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,55 @@
+#DATATYPES
+#integer
+x=23
+y=-868
+z=9898989
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+
+#float
+x=23.9
+y=-86.8
+z=989.8989
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+
+#string
+name1='nikki'
+name2='rashi'
+name3='anuu'
+print(name1,type(name1))
+print(name2,type(name2))
+print(name3,type(name3))
+print('"heyyyyy" i am niketa')
+
+#boolean
+is_coding = True
+are_u_bored = False
+print(is_coding, type(is_coding))
+print(are_u_bored, type(are_u_bored))
+
+#None
+x=None
+print(x,type(x))
+
+#list
+a = ['nikki',89,87.76]
+print(a,type(a))
+
+#tuple
+a = ('nikki',89,87.76)
+print(a,type(a))
+
+#set (unordered)
+a = {3,45,'hello','byee',87.8}
+print(a,type(a))
+
+#dictionary
+student_info = {
+    "name" : "rashi",
+    "age" : 19,
+    "course" : "b.tech"
+}
+print(student_info,type(student_info))
