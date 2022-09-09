c3b83eebcd5a22fd27c73ef810c0ec6550d2ba35
Niketatiwari <111993600+Niketatiwari@users.noreply.github.com>
2022-09-09 20:01:29 +0530

    Create condition.py

---
+++ condition.py
@@ -0,0 +1,17 @@
+num = int (input('enter a no.'))
+match num:
+    case 1:
+       print('sunday')
+    case 2:
+        print('monday')
+    case 3:
+       print('tuesday')
+    case 4:
+        print('wednesday')
+    case 5:
+       print('thursday')
+    case 6:
+        print('friday')
+    case 7:
+        print('saturday')      
+    
