1aeabd52879d6fc65e5bf50155bad6e226f7261d
Niketatiwari <111993600+Niketatiwari@users.noreply.github.com>
2022-11-14 14:28:54 +0530

    create other

---
+++ basics/others/info.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ")
+college = input("where do u study: ")
+city = input("city name: ")
+age = input("enter u: ")
+print(name, college, city, age)
+
+age = int(age) #typecasting
+print(age, type(age))
+
+a = int(input("Enter your no.: "))
+b = int (input("enter your no.: "))
+print(a + b)
